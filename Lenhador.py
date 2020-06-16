2ae2c46fec4fa3ed68455776bf8daa5636737c50
FernandoBichuette <fernandoba2@al.insper.edu.br>
2020-06-15 22:09:36 -0300

    final

--- Lenhador.py
+++ Lenhador.py
@@ -12,7 +12,7 @@ fnt_dir = path.join(path.dirname(__file__), 'font')
 WIDTH = 800
 HEIGHT = 600
 
-FPS = 60
+
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -21,41 +21,37 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 
-TAXA_VIDA = 200
-VIDA = 100
-
-GALHO_LISTA = [(450, HEIGHT - 120),
-               (200, HEIGHT - 270),
-               (200, HEIGHT - 420),
-               (450, HEIGHT - 570),
-               (450, HEIGHT - 720)]
-
-
-
-
+# Estados para controle do fluxo da aplicação
+INIT = 0
+GAME = 1
+QUIT = 2
 
 
 
 class Player(pygame.sprite.Sprite):
+    # Construtor da classe.
     def __init__(self):
+        
+        # Construtor da classe pai (Sprite).
         pygame.sprite.Sprite.__init__(self)
-
+        # Carregando a imagem de fundo.
         self.image = pygame.image.load(path.join(img_dir, 'LenhadorE.png')).convert_alpha()
+        # Diminuindo o tamanho da imagem.
         self.image = pygame.transform.scale(self.image, (120, 120))
-
+        # Deixando transparente.
         self.image.set_colorkey(BLACK)
-
+        # Detalhes sobre o posicionamento.
         self.rect = self.image.get_rect()
         self.rect.right = WIDTH/2 - 30
         self.rect.y = HEIGHT - 100
-
+        #Placar 
         self.pontos = 0
         self.score = 0
-
-        self.pos = 0
+        #Posicao do personagem
+        self.posicao = 0
 
     def update(self):
-        self.rect.x += self.pos
+        self.rect.x += self.posicao
         self.pontos += self.score
 
         if self.rect.right < WIDTH/2 - 30:
@@ -71,34 +67,40 @@ def load_assets(img_dir, fnt_dir):
     return assets
 
 class Galho(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    # Construtor da classe.
+    def __init__(self, posx, posy):
+        # Construtor da classe pai (Sprite).
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(
-            path.join(img_dir, 'Galho.png')).convert_alpha()
+        # Carregando a imagem de fundo.
+        self.image = pygame.image.load(path.join(img_dir, 'Galho.png')).convert_alpha()
+        # Deiposando transparente.
         self.image.set_colorkey(BLACK)
-
+        # Detalhes sobre o posicionamento.
         self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
+        self.rect.x = posx
+        self.rect.y = posy
 
     def update(self):
+        #Troca de lado o galho
         if self.rect.x == 200:
             self.image = pygame.image.load(path.join(img_dir, 'GalhoE.png')).convert_alpha()
-
             self.image.set_colorkey(BLACK)
         else:
             self.image = pygame.image.load(path.join(img_dir, 'GalhoD.png')).convert_alpha()
-
             self.image.set_colorkey(BLACK)
 
 
 class Tronco(pygame.sprite.Sprite):
+    # Construtor da classe.
     def __init__(self):
+        # Construtor da classe pai (Sprite).
         pygame.sprite.Sprite.__init__(self)
+        # Carregando a imagem de fundo.
         self.image = pygame.image.load(path.join(img_dir, 'Tronco.png')).convert_alpha()
         self.image = pygame.transform.scale(self.image, (300, 800))
+        # Deiposando transparente.
         self.image.set_colorkey(BLACK)
-
+        # Detalhes sobre o posicionamento.
         self.rect = self.image.get_rect()
         self.rect.centerx = 400
         self.rect.y = 0
--- main.py
+++ main.py
@@ -13,7 +13,6 @@ fnt_dir = path.join(path.dirname(__file__), 'font')
 WIDTH = 800
 HEIGHT = 600
 
-FPS = 60
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -39,54 +38,55 @@ imginicio = pygame.image.load(path.join(img_dir, 'Tela de inicio.png'))
 imginicio = pygame.transform.scale(imginicio, (800, 600))
 imgfundo_rect = imginicio.get_rect()
 
-
-
+# Inicialização do Pygame.
 pygame.init()
 
-
+# Carrega todos os assets uma vez só e guarda em um dicionário
 assets = load_assets(img_dir, fnt_dir)
-
 score_font = assets["score_font"]
 
+# Tamanho da tela.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Lumberjack")
 
-previous_time = pygame.time.get_ticks()
 
 all_sprites = pygame.sprite.Group()
 galho = pygame.sprite.Group()
 
+## Cria as sprites. O construtor será chamado automaticamente.
 tronco = Tronco()
 player = Player()
 all_sprites.add(tronco)
 all_sprites.add(player)
 
+#Pego da internet 
 font = pygame.font.SysFont("C:\Windows\Fonts\Arial.ttf", 72)
 text = font.render("Pontos: {0}".format(player.pontos), True, YELLOW)
 textRect = text.get_rect()
 textRect.center = (WIDTH // 2 - 300, 50)
-
+####
+#Pego da internet
 for branch in GALHO_LISTA:
     g = Galho(*branch)
     all_sprites.add(g)
     galho.add(g)
-
+####
 try:
     
     running = True
-    menu = True
+    main = True
 
-    while menu:
-        # clock.tick(FPS)
+    while main:
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                menu = False
+                main = False
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
-                    menu = False
+                    main = False
 
         screen.fill(BLACK)
         screen.blit(imginicio, imgfundo_rect)
@@ -100,22 +100,20 @@ try:
 
             if event.type == pygame.QUIT:
                 running = False
-
+            #Ação
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    player.pos = 170
+                    player.posicao = 170
                     player.image = pygame.image.load(path.join(img_dir, 'LenhadorD.png')).convert_alpha()
-
+            
                 if event.key == pygame.K_LEFT:
-                    player.pos = -220
+                    player.posicao = -220
                     player.image = pygame.image.load(path.join(img_dir, 'LenhadorE.png')).convert_alpha()
                     player.image = pygame.transform.scale(player.image, (120, 120))
 
-                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
-                    
+                if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:       
                     player.score = 1
-                    text = font.render("Pontos: {0}".format(
-                        player.pontos), True, YELLOW)
+                    text = font.render("Pontos: {0}".format(player.pontos), True, YELLOW)
                     textRect = text.get_rect()
                     textRect.center = (WIDTH // 2 - 300, 50)
 
@@ -126,22 +124,23 @@ try:
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    player.pos = 0
+                    player.posicao = 0
                     player.image = pygame.image.load(path.join(img_dir, 'LenhadorE.png')).convert_alpha()
                     player.image = pygame.transform.scale(player.image, (120, 120))
 
                 if event.key == pygame.K_RIGHT:
-                    player.pos = 0
+                    player.posicao= 0
                     player.image = pygame.image.load(path.join(img_dir, 'LenhadorD.png')).convert_alpha()
                     player.image = pygame.transform.scale(player.image, (120, 120))
 
+        #Pego da da internet
         if len(galho) < 4:
             lista_posicao = [200, 450]
             random.shuffle(lista_posicao)
             g = Galho(lista_posicao[0], 40)
             all_sprites.add(g)
             galho.add(g)
-
+        
         
 
         if VIDA <= 0:
@@ -162,6 +161,7 @@ try:
         screen.blit(text, textRect)
         all_sprites.draw(screen)
 
+        #Desenha os corações
         text_surface = score_font.render(chr(9829) * VIDA, True, RED)
         text_rect = text_surface.get_rect()
         text_rect.bottomleft = (WIDTH/2-100, HEIGHT - 530)
